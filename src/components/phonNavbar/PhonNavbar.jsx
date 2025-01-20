dc3e2d145216c37e7e1718938bae7706abaab5d5
unknown <aananmunna420@gmail.com>
2025-01-20 15:58:35 +0600

    adding mobile nav

--- src/components/phonNavbar/PhonNavbar.jsx
+++ src/components/phonNavbar/PhonNavbar.jsx
@@ -3,23 +3,23 @@ import 'react'
 const PhonNavbar = () => {
   return (
     <div className='fixed bottom-0 left-0 right-0 text-center p-2 bg-white flex items-center justify-around text-[#666666] border-t z-50 md:hidden'>
-        <a href='#' className='hover:text-blue-600'>
+        <a href='#' className='hover:bg-gradient-to-r from-cyan-500 to-blue-500 px-3 py-1 rounded-md hover:text-white'>
         <i className="bi bi-house text-2xl"></i>
-        <p className='text-xs uppercase'>Home</p>
+        <p className='text-xs uppercase'>Account</p>
         </a>
-        <a href='#' className='hover:text-blue-600'>
+        <a href='#' className='hover:bg-gradient-to-r from-cyan-500 to-blue-500 px-3 py-1 rounded-md hover:text-white'>
         <i className="bi bi-list-task text-2xl"></i>
         <p className='text-xs uppercase'>shop</p>
         </a>
-        <a href='#' className='hover:text-blue-600'>
+        <a href='#' className='hover:bg-gradient-to-r from-cyan-500 to-blue-500 px-3 py-1 rounded-md hover:text-white'>
         <i className="bi bi-person text-2xl"></i>
-        <p className='text-xs uppercase'>account</p>
+        <p className='text-xs uppercase'>Home</p>
         </a>
-        <a href='#' className='hover:text-blue-600'>
+        <a href='#' className='hover:bg-gradient-to-r from-cyan-500 to-blue-500 px-3 py-1 rounded-md hover:text-white'>
         <i className="bi bi-bag text-2xl"></i>
         <p className='text-xs uppercase'>cart</p>
         </a>
-        <a href='#' className='hover:text-blue-600'>
+        <a href='#' className='hover:bg-gradient-to-r from-cyan-500 to-blue-500 px-3 py-1 rounded-md hover:text-white'>
         <i className="bi bi-search text-2xl"></i>
         <p className='text-xs uppercase'>search</p>
         </a>
