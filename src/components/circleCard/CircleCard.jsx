aa1b5057d586bf2d24ca3ed7ab9892a95b05f59c
unknown <aananmunna420@gmail.com>
2025-01-20 16:28:28 +0600

    circle  card manage

--- src/components/circleCard/CircleCard.jsx
+++ src/components/circleCard/CircleCard.jsx
@@ -1,75 +1,97 @@
-import 'react'
+import React, { useEffect, useRef, useState } from "react";
+
+const sliderData = [
+  {
+    src: "https://images.othoba.com/images/thumbs/0584115_furniture-big-saving-days_120.jpeg",
+    title: "Furniture Big Saving Days",
+  },
+  {
+    src: "https://images.othoba.com/images/thumbs/0578718_special-weekly-offer_120.jpeg",
+    title: "Special Weekly Offer",
+  },
+  {
+    src: "https://images.othoba.com/images/thumbs/0667855_fashion_120.jpeg",
+    title: "Fashion",
+  },
+  {
+    src: "https://images.othoba.com/images/thumbs/0533529_electronics_120.webp",
+    title: "Electronic",
+  },
+  {
+    src: "https://images.othoba.com/images/thumbs/0676164_building-materials_120.jpeg",
+    title: "Building Materials",
+  },
+  {
+    src: "https://images.othoba.com/images/thumbs/0533528_home-living_120.webp",
+    title: "Home & Living",
+  },
+  {
+    src: "https://images.othoba.com/images/thumbs/0587377_riding-sports-fitness_120.webp",
+    title: "Riding, Sports & Fitness",
+  },
+  {
+    src: "https://images.othoba.com/images/thumbs/0616971_biking-accessories_120.jpeg",
+    title: "Biking & Accessories",
+  },
+  {
+    src: "https://images.othoba.com/images/thumbs/0680688_gift-packages_120.jpeg",
+    title: "Gift Packages",
+  },
+];
 
 const CircleCard = () => {
-  return (
-    <div className='container p-4 mx-auto'>
-        <div className="circleCard pl-[730px] h-full flex items-center justify-center lg:gap-5 overflow-x-scroll scroll-smooth transition-all [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
-            
-            <div className="card flex flex-col items-center justify-center text-center ">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0584115_furniture-big-saving-days_120.jpeg" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>Furniture Big Saving Days</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0578718_special-weekly-offer_120.jpeg" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>special weekly offer</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0667855_fashion_120.jpeg" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>fashion</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0533529_electronics_120.webp" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>electronic</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0676164_building-materials_120.jpeg" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>building materials</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0533528_home-living_120.webp" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>home & living</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0587377_riding-sports-fitness_120.webp" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>riding, sports & fitness</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0616971_biking-accessories_120.jpeg" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>biking & accessories</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0680688_gift-packages_120.jpeg" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>gift packages</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0667756_paints_120.jpeg" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>paints</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0533533_food-grocery_120.webp" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>food & grocery</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0577069_winter-appliances-fair_120.jpeg" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>winter appliances fair</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0634933_bags-wallets-belts_120.jpeg" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>bags, wallets & belts</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0684175_pharmacy_120.jpeg" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>pharmacy</a>
-            </div>
-            <div className="card flex flex-col items-center justify-center text-center">
-                <img className='max-w-none w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl' src="https://images.othoba.com/images/thumbs/0677188_household-festival_120.png" alt="" />
-                <a href="#" className='w-28 font-medium text-xs mt-3 leading-none uppercase hover:text-blue-500 transition-all'>household festival</a>
-            </div>
-           
-            
-        </div>
-    </div>
-  )
-}
+  const sliderRef = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-export default CircleCard
+  // Autoplay logic
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        (prevIndex + 1) % sliderData.length
+      );
+    }, 3000); // Change slides every 3 seconds
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
+  // Scroll to the current slide
+  useEffect(() => {
+    const slider = sliderRef.current;
+    if (slider) {
+      slider.scrollTo({
+        left: currentIndex * 120, // Adjust scroll offset based on image size
+        behavior: "smooth",
+      });
+    }
+  }, [currentIndex]);
+
+  return (
+    <div className="container p-4 mx-auto">
+      <div
+        ref={sliderRef}
+        className="circleCard flex items-center justify-start gap-3 lg:gap-5 overflow-x-scroll scroll-smooth transition-all [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]"
+      >
+        {sliderData.map((item, index) => (
+          <div
+            key={index}
+            className="card flex flex-col items-center justify-center text-center"
+          >
+            <img
+              className="max-w-none w-16 sm:w-20 lg:w-28 rounded-full cursor-pointer hover:scale-95 hover:border-8 transition-all border-[#fff] hover:shadow-2xl"
+              src={item.src}
+              alt={item.title}
+            />
+            <a
+              href="#"
+              className="w-20 sm:w-24 lg:w-28 font-medium text-xs mt-2 sm:mt-3 leading-none uppercase hover:text-blue-500 transition-all"
+            >
+              {item.title}
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CircleCard;
